0419fcb5afb6deb61c3bf639a55738338d339a6f
Cristian Monticelli <cristian.monticelli@studenti.isissgobetti.it>
2024-05-23 13:47:23 +0200

    2305

--- progetto/prove.py
+++ progetto/prove.py
@@ -1,12 +1,24 @@
 from flask import Flask, render_template
+import random
+import json
 
 app = Flask(__name__)
+FILE_PATH = "publicita.json" 
+pubblicita = []
 
+try:
+    with open(FILE_PATH, 'r') as file:
+        pubblicita = json.load(file)
+except FileNotFoundError:
+    pass
 
 @app.route('/')
-def index():
-    return render_template('iscrizione.html')
-
+def image_test():
+    immagine1 = random.choice(pubblicita)
+    immagine2 = random.choice(pubblicita)
+    while immagine1 == immagine2:
+        immagine2 = random.choice(pubblicita)
+    return render_template('prove.html', left=immagine1, right=immagine2)
 
 if __name__ == '__main__':
     app.run(debug=True)
